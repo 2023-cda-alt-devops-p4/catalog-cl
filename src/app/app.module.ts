73058dd46166799bc74affa20fe69ab27fa08f67
CamilleLafrance <cam.lafr@free.fr>
2023-09-19 16:23:51 +0200

    feat: create search bar component
    replace cards-list & search-bar directories in main-view directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { CardComponent } from './main-view/card/card.component';
+import { CardComponent } from './main-view/cards-list/card/card.component';
 import { MainViewComponent } from './main-view/main-view.component';
-import { CardsListComponent } from './cards-list/cards-list.component';
+import { CardsListComponent } from './main-view/cards-list/cards-list.component';
+import { SearchBarComponent } from './main-view/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CardsListComponent } from './cards-list/cards-list.component';
     NavigationComponent,
     CardComponent,
     MainViewComponent,
-    CardsListComponent
+    CardsListComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
