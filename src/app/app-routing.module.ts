fb6e6bd2b0340e6c861451ea49378f4068b2f87d
CamilleLafrance <cam.lafr@free.fr>
2023-09-19 16:29:38 +0200

    feat: add routes for main-view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainViewComponent } from './main-view/main-view.component';
 
-const routes: Routes = [{ path: '', redirectTo: '/home', pathMatch: 'full' }];
+const routes: Routes = [
+  { path: '', component: MainViewComponent },
+  { path: 'main-view', component: MainViewComponent },
+  { path: '**', redirectTo: '/main-view' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
