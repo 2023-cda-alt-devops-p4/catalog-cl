2d494c8379a4e6f8854e6ae63c55dcfbac6bfdef
CamilleLafrance <cam.lafr@free.fr>
2023-09-25 17:15:29 +0200

    feat: add button to card-details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainViewComponent } from './main-view/main-view.component';
+import { CardDetailsComponent } from './main-view/card-details/card-details.component';
 
 const routes: Routes = [
   { path: '', component: MainViewComponent },
   { path: 'main-view', component: MainViewComponent },
+  { path: 'card-details/:id', component: CardDetailsComponent },
   { path: '**', redirectTo: '/main-view' },
 ];
 
--- src/app/main-view/cards-list/cards-list.component.ts
+++ src/app/main-view/cards-list/cards-list.component.ts
@@ -12,45 +12,45 @@ export class CardsListComponent {
   constructor() {
     const card1 = new Card(
       1,
-      'Diagramme 1',
+      'Diagramme 1 deuxième ligne troisième ligne',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScVqPT4M7tFJYzra2KXua9kQsRkhxhfIlpSrpOuFRT7D3mirnDkmsHlPTcL21-xGHc10I&usqp=CAU',
       'Description du diagramme 1',
-      'Source du diagramme 1'
+      'Source 1'
     );
     const card2 = new Card(
       2,
       'Diagramme 2',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScVqPT4M7tFJYzra2KXua9kQsRkhxhfIlpSrpOuFRT7D3mirnDkmsHlPTcL21-xGHc10I&usqp=CAU',
       'Description du diagramme 2',
-      'Source du diagramme 2'
+      'Source 2'
     );
     const card3 = new Card(
       3,
       'Diagramme 3',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScVqPT4M7tFJYzra2KXua9kQsRkhxhfIlpSrpOuFRT7D3mirnDkmsHlPTcL21-xGHc10I&usqp=CAU',
       'Description du diagramme 3',
-      'Source du diagramme 3'
+      'Source 3'
     );
     const card4 = new Card(
       4,
       'Diagramme 4',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScVqPT4M7tFJYzra2KXua9kQsRkhxhfIlpSrpOuFRT7D3mirnDkmsHlPTcL21-xGHc10I&usqp=CAU',
       'Description du diagramme 4',
-      'Source du diagramme 4'
+      'Source 4'
     );
     const card5 = new Card(
       5,
       'Diagramme 5',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScVqPT4M7tFJYzra2KXua9kQsRkhxhfIlpSrpOuFRT7D3mirnDkmsHlPTcL21-xGHc10I&usqp=CAU',
       'Description du diagramme 5',
-      'Source du diagramme 5'
+      'Source 5'
     );
     const card6 = new Card(
       6,
       'Diagramme 6',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScVqPT4M7tFJYzra2KXua9kQsRkhxhfIlpSrpOuFRT7D3mirnDkmsHlPTcL21-xGHc10I&usqp=CAU',
       'Description du diagramme 6',
-      'Source du diagramme 6'
+      'Source 6'
     );
 
     this.cards.push(card1, card2, card3, card4, card5, card6);
