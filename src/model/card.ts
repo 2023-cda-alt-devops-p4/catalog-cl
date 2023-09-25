caca283f0676bf0f70a412b3ef320be908875cca
CamilleLafrance <cam.lafr@free.fr>
2023-09-25 17:43:07 +0200

    data: add diff types of UML & Merise diagrams

--- src/model/card.ts
+++ src/model/card.ts
@@ -2,6 +2,7 @@ export class Card {
   id: number;
   name: string;
   image: string;
+  sourceImage: string;
   description: string;
   source: string;
 
@@ -9,12 +10,14 @@ export class Card {
     id: number,
     name: string,
     image: string,
+    sourceImage: string,
     description: string,
     source: string
   ) {
     this.id = id;
     this.name = name;
     this.image = image;
+    this.sourceImage = sourceImage;
     this.description = description;
     this.source = source;
   }
