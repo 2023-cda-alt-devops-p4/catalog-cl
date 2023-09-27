db101912150015a2423a9950e5e7e3ee45f6fac4
CamilleLafrance <cam.lafr@free.fr>
2023-09-27 15:39:19 +0200

    feat: make navbar more responsive
    add logo on left side
    change menu burger (mobile)
    add action on menu burger: show or not to show nav items

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
