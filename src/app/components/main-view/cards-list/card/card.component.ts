6a0af79afd856c7b6f4627b9af5eeb59118d93ea
CamilleLafrance <cam.lafr@free.fr>
2023-09-29 21:45:14 +0200

    feat: add background images to cards
    remove scrollbar display
    adapt cards display

--- src/app/components/main-view/cards-list/card/card.component.ts
+++ src/app/components/main-view/cards-list/card/card.component.ts
@@ -4,8 +4,20 @@ import { Card } from 'src/app/models/card';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
   @Input() diagram!: Card;
+
+  /* RANDOM BACKGROUND */
+  getBackgroundImage(diagramId: number): string {
+    const backgroundImages = [
+      'assets/backgrounds/eva-bronzini.avif',
+      'assets/backgrounds/henry-_-co.avif',
+      'assets/backgrounds/la-miko.avif', 
+      'assets/backgrounds/madison-inouye.avif',
+      'src/assets/backgrounds/sasha-martynov.avif',
+    ];
+    return backgroundImages[diagramId % 4];
+  }
 }
